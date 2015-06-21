deaf2e19ff529049bc160c66d6f38e2312316ca9
Ahmet Cecen <ahmetcecen@gmail.com>
2015-06-21 02:08:21 -0400

    Update

--- TwoPointMaster.m
+++ TwoPointMaster.m
@@ -1,34 +1,44 @@
 function GG = TwoPointMaster(memtype,corrtype,cutoff,varargin)
-% FFT Convolution Wrapper for Two-Point Statistics Calculations. A by-product of ongoing computational
-% materials science research at MINED@Gatech.(http://mined.gatech.edu/)
+% FFT Convolution Wrapper for Two-Point Statistics Calculations. 
+% A by-product of ongoing computational materials science research 
+% at MINED@Gatech.(http://mined.gatech.edu/)
 %
-% This function calculates VECTOR COUNTS for utilization in 2-point statistics calculations. A proper statistics
-% calculation would use this function TWICE: once for the numerator and once for the denominator. 
+% This function calculates VECTOR COUNTS for utilization in 2-point 
+% statistics calculations. A proper statistics calculation would use this 
+% function TWICE: once for the numerator and once for the denominator. 
 %
-% GG = TwoPointMaster('full','auto',cutoff,H1) calculates the vector counts for the auto correlation of H1, 
-% with a maximum vector length of "cutoff". 
+% GG = TwoPointMaster('full','auto',cutoff,H1) calculates the vector counts
+% for the auto correlation of H1,with a maximum vector length of "cutoff". 
 %
-% GG = TwoPointMaster('full','cross',cutoff,H1,H2) calculates the vector counts for the  cross correlation of H1 and H2, 
-% with a maximum vector length of "cutoff". 
+% GG = TwoPointMaster('full','cross',cutoff,H1,H2) calculates the vector 
+% counts for the  cross correlation of H1 and H2, with a maximum vector 
+% length of "cutoff". 
 % 
-% GG = TwoPointMaster('patched','auto',cutoff,DataFile,winmulti) calculates the vector counts for the auto correlation of H1, 
-% with a maximum vector length of "cutoff". However this version uses the patching convolution method to save memory,
-% sacrificing computation time. A balance between memory and computation time can be struck using the winmulti parameter.
-% A winmulti of 1 will result in the minimum memory calculation, and will take considerably (sometimes an order of magnitude)
-% longer to calculate, whereas a winmulti of 3 will take roughly 8 gb of memory, resulting in only twice the computational time
-% for most practical cases.
+% GG = TwoPointMaster('patched','auto',cutoff,DataFile,winmulti) calculates
+% the vector counts for the auto correlation of H1,with a maximum vector 
+% length of "cutoff". However this version uses the patching convolution 
+% method to save memory, sacrificing computation time. A balance between 
+% memory and computation time can be struck using the winmulti parameter.
+% A winmulti of 1 will result in the minimum memory calculation, and will 
+% take considerably (sometimes an order of magnitude) longer to calculate, 
+% whereas a winmulti of 3 will take roughly 8 gb of memory, resulting in 
+% only twice the computational time for most practical cases.
 %
-% %GG = TwoPointMaster('patched','cross',cutoff,DataFile,winmulti,DataFile2) calculates the vector counts for the cross correlation of H1 and H2, 
-% with a maximum vector length of "cutoff". However this version uses the patching convolution method to save memory,
-% sacrificing computation time. A balance between memory and computation time can be struck using the winmulti parameter.
-% A winmulti of 1 will result in the minimum memory calculation, and will take considerably (sometimes an order of magnitude)
-% longer to calculate, whereas a winmulti of 3 will take roughly 11 gb of memory, resulting in only twice the computational time
-% for most practical cases.
+% %GG = TwoPointMaster('patched','cross',cutoff,DataFile,winmulti,DataFile2)
+% calculates the vector counts for the cross correlation of H1 and H2, 
+% with a maximum vector length of "cutoff". However this version uses the 
+% patching convolution method to save memory, sacrificing computation time. 
+% A balance between memory and computation time can be struck using the 
+% winmulti parameter. A winmulti of 1 will result in the minimum memory 
+% calculation, and will take considerably (sometimes an order of magnitude)
+% longer to calculate, whereas a winmulti of 3 will take roughly 11 gb of 
+% memory, resulting in only twice the computational time for most practical 
+% cases.
 %
 % Author: Ahmet Cecen
 % Contact: ahmetcecen@gmail.com
 %
-  
+
 switch memtype
 	
 	case 'full'
@@ -36,8 +46,15 @@ switch memtype
 		switch corrtype 
 
 			case 'auto'
+                
 				H1 = varargin{1};
-				GG = fftshift(ifftn(fftn(H1).*conj(fftn(H1))));
+                
+                H1 = fftn(H1);
+                H1 = H1.*conj(H1);
+                H1 = ifftn(H1);
+                H1 = fftshift(H1);
+                
+                GG = H1;
 				
 				if ismatrix(H1)
 					GG = GG((floor((size(GG,1)/2)+1)-(cutoff-1)):(floor((size(GG,1)/2)+1)+(cutoff-1)),...
@@ -51,9 +68,19 @@ switch memtype
 				end
 				
 			case 'cross'
+                
 				H1 = varargin{1};
 				H2 = varargin{2};
-				GG = fftshift(ifftn(fftn(H1).*conj(fftn(H2))));			
+
+                H1 = fftn(H1);
+                H1 = conj(H1);
+                H2 = fftn(H2);
+                H1 = H1.*H2;
+                clearvars H2;
+                H1 = ifftn(H1);
+                H1 = fftshift(H1);
+                
+                GG = H1;		
 							
 				if ismatrix(H1)
 					GG = GG((floor((size(GG,1)/2)+1)-(cutoff-1)):(floor((size(GG,1)/2)+1)+(cutoff-1)),...
@@ -117,6 +144,10 @@ switch memtype
 		xwinlist=[xwinsize*ones(xbc-1,1);(xwinsize+xbuftra)];
 		ywinlist=[ywinsize*ones(ybc-1,1);(ywinsize+ybuftra)];
 		
+        % Progress Bar Initialize
+        progress=0;
+        reverseStr = '';
+        
 		% Main Loop
 		
 		switch corrtype 
@@ -142,7 +173,17 @@ switch memtype
 							G = G((floor((size(G,1)/2)+1)-(cutoff-1)):(floor((size(G,1)/2)+1)+(cutoff-1)),...
 								(floor((size(G,2)/2)+1)-(cutoff-1)):(floor((size(G,2)/2)+1)+(cutoff-1)));
 						 
-							GG = GG + G;           
+							GG = GG + G;     
+                            
+                            if xx==1 && yy==1
+                                est=toc;
+                                fprintf('Estimated Completion = %.2f minutes - ',(xbc*ybc-1)*est/60);
+                            end              
+                            
+                            % Progress Bar
+                            fprintf(reverseStr);
+                            msg=fprintf('Progress = %.2f %%',100*progress/(xbc*ybc));                                
+                            reverseStr = repmat(sprintf('\b'), 1, msg);                                
 			
 						end
 					end
@@ -152,6 +193,10 @@ switch memtype
 					for xx=1:xbc
 						for yy=1:ybc
 							for zz=1:zbc
+                                
+                                tic;
+                                
+                                progress=progress+1;
 							
 								% Grab H1 Window
 								HH1 = m.H1(((sum(xwinlist(1:xx)))-xwinlist(xx)+1):(sum(xwinlist(1:xx))+2*(cutoff-1)),...
@@ -168,7 +213,17 @@ switch memtype
 									(floor((size(G,2)/2)+1)-(cutoff-1)):(floor((size(G,2)/2)+1)+(cutoff-1)),...
 									(floor((size(G,3)/2)+1)-(cutoff-1)):(floor((size(G,3)/2)+1)+(cutoff-1)));
 							 
-								GG = GG + G;           
+								GG = GG + G;  
+                                
+                                if xx==1 && yy==1 && zz==1
+                                    est=toc;
+                                    fprintf('Estimated Completion = %.2f minutes - ',(xbc*ybc*zbc-1)*est/60);
+                                end
+                                
+                                % Progress Bar
+                                fprintf(reverseStr);
+                                msg=fprintf('Progress = %.2f %%',100*progress/(xbc*ybc*zbc));                                
+                                reverseStr = repmat(sprintf('\b'), 1, msg);
 				
 							end
 						end
@@ -207,8 +262,18 @@ switch memtype
 							G = G((floor((size(G,1)/2)+1)-(cutoff-1)):(floor((size(G,1)/2)+1)+(cutoff-1)),...
 								(floor((size(G,2)/2)+1)-(cutoff-1)):(floor((size(G,2)/2)+1)+(cutoff-1)));
 						 
-							GG = GG + G;           
-								
+							GG = GG + G; 
+                            
+                            if xx==1 && yy==1
+                                est=toc;
+                                fprintf('Estimated Completion = %.2f minutes - ',(xbc*ybc-1)*est/60);
+                            end                            
+
+                            % Progress Bar
+                            fprintf(reverseStr);
+                            msg=fprintf('Progress = %.2f %%',100*progress/(xbc*ybc));                                
+                            reverseStr = repmat(sprintf('\b'), 1, msg);                            
+                            
 						end
 					end					
 				
@@ -238,7 +303,17 @@ switch memtype
 									(floor((size(G,2)/2)+1)-(cutoff-1)):(floor((size(G,2)/2)+1)+(cutoff-1)),...
 									(floor((size(G,3)/2)+1)-(cutoff-1)):(floor((size(G,3)/2)+1)+(cutoff-1)));
 							 
-								GG = GG + G;           
+								GG = GG + G;      
+  
+                                if xx==1 && yy==1 && zz==1
+                                    est=toc;
+                                    fprintf('Estimated Completion = %.2f minutes - ',(xbc*ybc*zbc-1)*est/60);
+                                end                                
+                                
+                                % Progress Bar
+                                fprintf(reverseStr);
+                                msg=fprintf('Progress = %.2f %%',100*progress/(xbc*ybc*zbc));                                
+                                reverseStr = repmat(sprintf('\b'), 1, msg);                                
 								
 							end
 						end
